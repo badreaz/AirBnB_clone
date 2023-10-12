63c8d89df025de409cb79579211032bb896894f6
sayv <savegawugah01@gmail.com>
2023-10-12 10:06:38 +0000

    fixed circular imports

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from uuid import uuid4
 from models import storage
 
-
 fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
 
     def all(self):
         """Returns dictionary of objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj and obj class name as key"""
@@ -23,11 +23,19 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to JSON path"""
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = value.to_dict()
         with open(FileStorage.__file_path, "w") as fd:
-            json.dump(FileStorage.__objects, fd)
+            json.dump(obj_dict, fd)
 
     def reload(self):
         """Deserialize JSON file to __objects if it exists"""
         try:
             with open(FileStorage.__file_path, "r") as fd:
-                FileStorage.__objects = json.load(fd)
+                obj_dict = json.load(fd)
+            for key, value in obj_dict.items():
+                class_name = value["__class__"](**value)
+                FileStorage.__objects[key] = class_name
+        except FileNotFoundError:
+            pass
