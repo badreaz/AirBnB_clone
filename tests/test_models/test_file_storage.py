e697466f3666eaadb06429fe1e700724792b8995
badreaz <baz.23.2019@gmail.com>
2023-10-15 19:44:07 +0400

    create test_file_storage.py

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" FileStorage class tests module """
+import unittest
+from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = BaseModel()
+        self.key = "BaseModel." + str(self.obj.id)
+
+    def test_is_instance(self):
+        self.assertIsInstance(storage, FileStorage)
+
+    def test_new(self):
+        objects = storage.all()
+        self.assertEqual(objects[self.key].__dict__, self.obj.__dict__)
+
+    def test_save(self):
+        self.obj.name = "name"
+        storage.save()
+        objects = storage.all()
+        self.assertEqual(objects[self.key].__dict__, self.obj.__dict__)
