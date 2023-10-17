5d505e734d8bfaaf8a07fd5bc786e378e3a777fb
sayv <savegawugah01@gmail.com>
2023-10-17 21:06:00 +0000

    fix typo in place.py and updated test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ import unittest
 from models.engine.file_storage import FileStorage
 from models import storage
 from models.base_model import BaseModel
+import os
 
 
 class TestFileStorage(unittest.TestCase):
@@ -11,6 +12,12 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         self.obj = BaseModel()
         self.key = "BaseModel." + str(self.obj.id)
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
     def test_is_instance(self):
         self.assertIsInstance(storage, FileStorage)
@@ -24,3 +31,9 @@ class TestFileStorage(unittest.TestCase):
         storage.save()
         objects = storage.all()
         self.assertEqual(objects[self.key].__dict__, self.obj.__dict__)
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_reload(self):
+        self.storage = FileStorage()
+        self.storage.reload()
+        self.assertTrue(True)
